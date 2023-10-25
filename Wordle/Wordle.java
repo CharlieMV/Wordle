dc5aabe29a63b3c1f0c9af6bd6e8c68802cee85e
CharlieMV <143232951+CharlieMV@users.noreply.github.com>
2023-10-24 20:18:47 -0700

    Add files via upload

--- Wordle/Wordle.java
+++ Wordle/Wordle.java
@@ -41,7 +41,7 @@ public class Wordle
 	 *	show				-	when true, will print the current word to the terminal
 	 *	readyForKeyInput    -	when true, will accept keyboard input, when false,
 	 *							will not accept keyboard input.
-	 *	readyForMouseInput  -	when true, will accept mouse input, when false,
+	 *	readyForMouseInput  -	when true, will accept mouse input, when false,+
 	 *							will not accept mouse input.
 	 *	activeGame          -	when false, will only accept action on the RESET button.
 	 */
@@ -93,7 +93,7 @@ public class Wordle
 	 *	The main method, to run the program.  The constructor is called, so that
 	 *	all of the fields are initialized.  The canvas is set up, and the GUI
 	 *	(the game of Wordle) runs.
-	 *	THIS METHOD IS INCOMPLETE.
+	 *	THIS METHOD IS COMPLETE.
 	 */
 	public static void main(String[] args)
 	{
@@ -235,7 +235,7 @@ public class Wordle
 	 *	inAllowedWordFile will be called for this task.  If the guess in letters
 	 *	does not exist in the text file, a message is displayed to the user in the
 	 *	form of a JOptionPane with JDialog.
-	 *	THIS METHOD IS INCOMPLETE.
+	 *	THIS METHOD IS COMPLETE. 10/24/23
 	 */
 	public void processGuess ( )
 	{
@@ -245,53 +245,137 @@ public class Wordle
 		int guessNumber = 0;
 		for(int i = 0; i < wordGuess.length; i++)
 		{
-			System.out.println(wordGuess[i] + " " + wordGuess[i].length() +
-										inAllowedWordFile(wordGuess[i]));
 			if(wordGuess[i].length() == 5 && inAllowedWordFile(wordGuess[i]))
 			{
 				guessNumber = i + 1;
 			} 
-			// if guess is not in words5allowed.txt then print dialog box
-			else if (wordGuess[i].length() == 5 && !inAllowedWordFile(wordGuess[i])) {
-				JOptionPane pane = new JOptionPane(wordGuess[i] + 
-											" is not a valid word");
-				JDialog d = pane.createDialog(null, "INVALID INPUT");
-				d.setLocation(365,250);
-				d.setVisible(true);
-			}
 		}
 		
 		wordGuess[guessNumber] = letters.toUpperCase();
 		letters = "";
+		
+		// if guess is not in words5allowed.txt then print dialog box
+		
+		if (wordGuess[guessNumber].length() == 5 && 
+							!inAllowedWordFile(wordGuess[guessNumber])) {
+			JOptionPane pane = new JOptionPane(wordGuess[guessNumber] + 
+										" is not a valid word");
+			JDialog d = pane.createDialog(null, "INVALID INPUT");
+			d.setLocation(365,250);
+			d.setVisible(true);
+			wordGuess[guessNumber] = "";
+		}
 	}
 	
 	/** 
 	 *	Draws the entire game panel.  This includes the guessed words, the current
 	 *	word being guessed, and all of the letters in the "keyboard" at the bottom
 	 *	of the gameboard.  The correct colors will need to be chosen for every letter.
-	 *	THIS METHOD IS INCOMPLETE.
+	 *	THIS METHOD IS COMPLETE. 10/24/23
 	 */
 	public void drawPanel ( )
 	{
+		// Keep track of keyboard colors before arranging to layout
+		int[] tmpKeyColors = new int[29];
 		StdDraw.clear(StdDraw.WHITE);
-		
+		int[][] colors = new int[6][5];
 		// Determine color of guessed letters and draw backgrounds
 	 	// 0 for not checked yet, 1 for no match, 2 for partial, 3 for exact
+	 	for(int row = 0; row < 6; row++)
+		{
+			for(int col = 0; col < 5; col++)
+			{
+				// If letter is not ""
+				if(wordGuess[row].length() != 0) {
+					// Default to grey
+					colors[row][col] = 1;
+					// Check all characters of the guess for yellow
+					for(int i = 0; i < word.length(); i++) 
+						if(word.toUpperCase().charAt(i) == 
+								wordGuess[row].toUpperCase().charAt(col))
+							colors[row][col] = 2;
+					// Green will override yellow
+					if(wordGuess[row].toUpperCase().charAt(col) == 
+										word.toUpperCase().charAt(col)) 
+						colors[row][col] = 3;
+				// If letter is ""
+				} else colors[row][col] = 0;
+			}
+		}
+	 	
 		// draw guessed letter backgrounds
 		
 		for(int row = 0; row < 6; row++)
 		{
 			for(int col = 0; col < 5; col++)
 			{
-				if(wordGuess[row].length() != 0)											//  THIS METHOD IS INCOMPLETE.
-				{
-					StdDraw.picture(209 + col * 68, 650 - row * 68, "letterFrameDarkGray.png");
-				}
-				else
-				{
-					StdDraw.picture(209 + col * 68, 650 - row * 68, "letterFrame.png");
-				}
+				if (wordGuess[row].length() != 0)
+					switch(colors[row][col]) {
+						case 1: 
+						tmpKeyColors[wordGuess[row].toUpperCase().charAt(col) 
+																- 'A'] = 1;
+						break; case 2:
+						tmpKeyColors[wordGuess[row].toUpperCase().charAt(col) 
+																- 'A'] = 2;
+						break; case 3:
+						tmpKeyColors[wordGuess[row].toUpperCase().charAt(col) 
+																- 'A'] = 3;
+						break; default:
+						tmpKeyColors[wordGuess[row].toUpperCase().charAt(col) 
+																- 'A'] = 0;
+					}
+				switch(colors[row][col]) {
+						case 1: 
+						StdDraw.picture(209 + col * 68, 650 - row * 68, 
+												"letterFrameDarkGray.png");
+						break; case 2:
+						StdDraw.picture(209 + col * 68, 650 - row * 68, 
+												"letterFrameYellow.png");
+
+						break; case 3:
+						StdDraw.picture(209 + col * 68, 650 - row * 68, 
+												"letterFrameGreen.png");
+
+						break; default:
+						StdDraw.picture(209 + col * 68, 650 - row * 68, 
+												"letterFrame.png");
+					}
 			}
+		}
+		
+		// Transpose a = 0; b = 1; ... z = 25 to QWERTY Q = 0; W = 1, etc.
+		for(int i = 0; i < 27; i++) {
+			char letter = 'a';
+			switch (i) {
+				case 0: letter = 'Q'; break;
+				case 1: letter = 'W'; break;
+				case 2: letter = 'E'; break;
+				case 3: letter = 'R'; break;
+				case 4: letter = 'T'; break;
+				case 5: letter = 'Y'; break;
+				case 6: letter = 'U'; break;
+				case 7: letter = 'I'; break;
+				case 8: letter = 'O'; break;
+				case 9: letter = 'P'; break;
+				case 10: letter = 'A'; break;
+				case 11: letter = 'S'; break;
+				case 12: letter = 'D'; break;
+				case 13: letter = 'F'; break;
+				case 14: letter = 'G'; break;
+				case 15: letter = 'H'; break;
+				case 16: letter = 'J'; break;
+				case 17: letter = 'K'; break;
+				case 18: letter = 'L'; break;
+				case 20: letter = 'Z'; break;
+				case 21: letter = 'X'; break;
+				case 22: letter = 'C'; break;
+				case 23: letter = 'V'; break;
+				case 24: letter = 'B'; break;
+				case 25: letter = 'N'; break;
+				case 26: letter = 'M'; break;
+			}
+			if (letter != 'a')
+				keyBoardColors[i] = tmpKeyColors[letter - 'A'];
 		}
 		
 		// draw Wordle board
@@ -309,12 +393,21 @@ public class Wordle
 			{
 				StdDraw.picture(pair[0], pair[1], "keyBackgroundBig.png");		
 			}						
-			//  This needs to be modified a great deal,
-			//  so that the correct colors show up.
 			else
 			{
-				StdDraw.picture(pair[0], pair[1], "keyBackground.png");
+				switch (keyBoardColors[place]) {
+					case 1:
+					StdDraw.picture(pair[0], pair[1], "keyBackgroundDarkGray.png");
+					break; case 2: 
+					StdDraw.picture(pair[0], pair[1], "keyBackgroundYellow.png");
+					break; case 3:
+					StdDraw.picture(pair[0], pair[1], "keyBackgroundGreen.png");
+					break; default: 
+					StdDraw.picture(pair[0], pair[1], "keyBackground.png");
+					break;
+				}
 			}
+			
 			StdDraw.setPenColor(StdDraw.BLACK);
 			StdDraw.text(pair[0], pair[1], Constants.KEYBOARD[place]);
 			place++;
@@ -343,7 +436,6 @@ public class Wordle
 		int guessNumber = 0;
 		for(int i = 0; i < wordGuess.length; i++)
 		{
-			System.out.println(wordGuess[i] + wordGuess[i].length());
 			if(wordGuess[i].length() > 0)
 			{
 				for(int j = 0; j < wordGuess[i].length(); j++)
@@ -381,7 +473,7 @@ public class Wordle
 		}
 		
 		// declare the winner by matching the word
-		if(lastWord.equals(word))
+		if(lastWord.toUpperCase().equals(word.toUpperCase()))
 		{
 			activeGame = false;
 			JOptionPane pane = new JOptionPane(lastWord + " is the word!  Press RESET to begin again");
